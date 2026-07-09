edc93cbada0a300a038c355b1c17ee0e7f40317e
NORMAL-EX <ce-ramos@qq.com>
2026-07-09 18:25:12 +0800

    docs: rename start sidebar section

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -48,7 +48,7 @@ interface MarkdownModule {
 // 侧边栏：中英文链接一致（语言无关），仅文字不同。
 export const sidebarZh: SidebarItem[] = [
   {
-    text: '开始',
+    text: '开始使用',
     items: [{ text: '写在前面', link: '/docs/start/start' }],
   },
   {
@@ -95,7 +95,7 @@ export const sidebarZh: SidebarItem[] = [
 
 export const sidebarEn: SidebarItem[] = [
   {
-    text: 'Getting Started',
+    text: 'Start Using',
     items: [{ text: 'Introduction', link: '/docs/start/start' }],
   },
   {
